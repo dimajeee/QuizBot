44f188b825318426bff7390454cc1a3ce4699a96
Dmitry <dsterligov2004@mail.ru>
2023-11-21 09:45:35 +0500

    теперь база personbase является static, это значит, что база одна для всех экземпляров класса Person изменен конструктор с параметром, теперь он не дублирует код из конструктора без парамертов, а просто вызывает его

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -3,33 +3,25 @@ package org.example;
 import java.util.ArrayList;
 
 public class Person {
-    private ArrayList <Person> personBase;
+    private static final ArrayList <Person> personBase = new ArrayList<Person>();
     private String chatID;
-    // private Response response;
     private boolean BotCondition = false;
-    private DialogContext dialogContext;
-    // private Request request;
-    private GameQuiz gameQuiz;
-    private QuizResponse quizResponse;
-    public Person(String chatID) {
-        this.chatID = chatID;
-        gameQuiz = new GameQuiz();
-        // response = new Response();
-        dialogContext = new DialogContext();
-        quizResponse = new QuizResponse();
-        quizResponse.updateQuizBase();
-        // request = new Request(req);
-    }
+    private final DialogContext dialogContext;
+    private final GameQuiz gameQuiz;
+    private final QuizResponse quizResponse;
     public Person (){
-      personBase=new ArrayList<>();
       gameQuiz = new GameQuiz();
       dialogContext = new DialogContext();
       quizResponse = new QuizResponse();
       quizResponse.updateQuizBase();
     }
-    public Person getPersonFromBase (int i){
-        return personBase.get(i);
+    public Person(String chatID){
+        this(); // вызов предыдущего конструктора
+        this.chatID = chatID;
     }
+//    public Person getPersonFromBase (int i){
+//        return personBase.get(i);
+//    }
     public Person getPersonByChatID(String chatID) {
         for (Person person : personBase) {
             if (person.getChatID().equals(chatID)) {
@@ -43,21 +35,15 @@ public class Person {
     public void setBotCondition(boolean botCondition) {
         BotCondition = botCondition;
     }
-    public GameQuiz getGameQuiz() {
-        return gameQuiz;
-    }
-    public QuizResponse getQuizResponse() {
-        return quizResponse;
-    }
-//    public Request getRequest() {
-//        return request;
-//    }
-//    public Response getResponse() {return response;
-//    }
-    public String getChatID() {
-        return chatID;
-    }
     public boolean getBotCondition() {
         return BotCondition;
     }
+    public GameQuiz getGameQuiz() {return gameQuiz;}
+    public QuizResponse getQuizResponse() {
+        return quizResponse;
+    }
+    public String getChatID() {
+        return chatID;
+    }
+
 }
